bd2898e13ad52bd906a356ab0479818bc83c5063
Eric Chan <ericchan277@gmail.com>
2018-02-08 10:54:14 -0800

    Created board, paddles, balls and line svg

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import Game from './partials/Game'
 // create a game instance
 const game = new Game('game', 512, 256);
 
+
 (function gameLoop() {
     game.render();
     requestAnimationFrame(gameLoop);
+    
 })();
