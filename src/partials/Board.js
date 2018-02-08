c9d3cfc807928de0d57de392664a3617f856dada
Eric Chan <ericchan277@gmail.com>
2018-02-08 13:12:42 -0800

    Added line attributes

--- src/partials/Board.js
+++ src/partials/Board.js
@@ -5,7 +5,25 @@ export default class Board {
       this.width = width;
       this.height = height;
     }
-    render() {
-      //...
+    render(svg) {
+        let rect = document.createElementNS(SVG_NS, 'rect');
+        rect.setAttributeNS(null, 'fill', '#353535');
+        rect.setAttributeNS(null, 'width', this.width);
+        rect.setAttributeNS(null, 'height', this.height);
+        rect.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+        
+        
+        let line = document.createElementNS(SVG_NS, 'line');
+        line.setAttributeNS(null, 'x1', (this.width/2));
+        line.setAttributeNS(null, 'y1', '0');
+        line.setAttributeNS(null, 'x2', (this.width/2));
+        line.setAttributeNS(null, 'y2', this.height);
+        line.setAttributeNS(null, 'stroke', 'white');
+        line.setAttributeNS(null, 'stroke-width', '4');
+        line.setAttributeNS(null, 'stroke-array', '20, 18');
+        
+        svg.appendChild(rect);
+        svg.appendChild(line);
+
     }
   }
