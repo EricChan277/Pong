4b262d71511dec4c17e0858b6b0a36d10a5d7898
Eric Chan <ericchan277@gmail.com>
2018-02-08 13:24:43 -0800

    Linked Board.js with Game.js

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,4 +1,5 @@
 import {SVG_NS} from '../settings';
+import Board from './Board';
 
 export default class Game {
 
@@ -9,16 +10,22 @@ export default class Game {
 		this.gameElement = document.getElementById(this.element);
 
 		// Other code goes here...
+		this.board = new Board(this.width, this.height);
 	}
 
 	render() {
 		// More code goes here...
+
 		this.gameElement.innerHTML ='';
 		let svg = document.createElementNS(SVG_NS, 'svg');
 		svg.setAttributeNS(null, 'width', this.width);
 		svg.setAttributeNS(null, 'height', this.height);
 		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+		
+		this.board.render(svg);
+		
 		this.gameElement.appendChild(svg);
+
 	}
 
 }
