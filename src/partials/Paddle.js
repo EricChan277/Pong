0957d243f2da59d09d83869b12a254b8239b2112
Eric Chan <ericchan277@gmail.com>
2018-02-08 15:01:38 -0800

    Added movement to paddles

--- src/partials/Paddle.js
+++ src/partials/Paddle.js
@@ -2,17 +2,41 @@ import {SVG_NS} from '../settings';
 
 export default class Paddle {
     constructor(boardHeight, width, height, x, y, up, down) {
-      this.boardHeight = boardHeight;
-      this.width = width;
-      this.height = height;
-      this.x = x;
-      this.y = y;
-      this.speed = 10;
-      this.score = 0;
-    //   this.color = color;
-      this.up = up;
-      this.down = down;
-    }
+        this.boardHeight = boardHeight;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.speed = 10;
+        this.score = 0;
+        //   this.color = color;
+
+        //constructor start
+        document.addEventListener('keydown', event => {
+            switch (event.key) {
+                case up:
+                    this.up();
+                    break;
+                case down:
+                    this.down();
+                    break;
+            }
+        });
+    }//constructor end
+
+        //constructor start 
+
+        up() {
+            console.log('Up');
+            this.y = this.y - this.speed;
+        }
+
+        down() {
+            console.log('Down');
+            this.y = this.y + this.speed;
+        }
+
+
 
     //...
     render(svg) {
@@ -22,7 +46,6 @@ export default class Paddle {
         rect.setAttributeNS(null, 'height', this.height);
         rect.setAttributeNS(null, 'x', this.x);
         rect.setAttributeNS(null, 'y', this.y);
-
 
         svg.appendChild(rect);
 
