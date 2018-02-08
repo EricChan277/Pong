ea5c2c8d96bec0ac2d07a0a0f2037a211b82ab98
Eric Chan <ericchan277@gmail.com>
2018-02-08 11:50:28 -0800

    Created Board.js, edited JSON file

---
+++ src/partials/Board.js
@@ -0,0 +1,11 @@
+import {SVG_NS} from '../settings';
+
+export default class Board {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+    render() {
+      //...
+    }
+  }
